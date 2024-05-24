b51224df124b6657ec87240c5a6890e22f09d45e
Gabrielle Oliveira <oliveiragabrielle662@gmail.com>
2024-05-24 12:48:32 -0300

    notifications service

---
+++ src/application/use-cases/count-recipient-notifications.spec.ts
@@ -0,0 +1,23 @@
+import { InMemoryNotificationsRepository } from "@test/repositories/in-memory-notifications-repository"
+import { Notification } from "@application/entities/notification"
+import { Content } from "@application/entities/content"
+import { CountRecipientNotification } from "./count-recipient-notifications"
+import { makeNotification } from "@test/factories/notification-factory"
+
+
+describe("Count recipient notifications", () => {
+    it('should be able to count recipient notifications', async () => {
+        const notificationsRepository = new InMemoryNotificationsRepository()
+        const countRecipientNotifications = new CountRecipientNotification(notificationsRepository)
+
+        await notificationsRepository.create(makeNotification({ recipientId: 'recipient-1' }))
+        await notificationsRepository.create(makeNotification({ recipientId: 'recipient-1' }))
+        await notificationsRepository.create(makeNotification({ recipientId: 'recipient-2' }))
+
+        const {count} = await countRecipientNotifications.execute({
+            recipientId: 'example-id-recipient-1'
+        })
+
+        expect(count).toEqual(2)
+    })
+})
